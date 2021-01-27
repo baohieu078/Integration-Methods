15b5da8600e3b37d4ca6e954bc792e72140afa76
baohieu078 <lam-bao-hieu.truong@hsrw.org>
2021-01-27 23:40:15 +0100

    Initial commit

---
+++ src/Integral/Function.java
@@ -0,0 +1,59 @@
+package Integral;
+
+
+import static java.lang.Math.*;
+
+public class Function {
+    static double pi = 3.14;
+
+    public static double delta(double a, double b, double n){
+        return (a - b)/n;
+    }
+
+    public static double function1(double x){
+        return ((4*x - 1)/ (sqrt(2*x+1)+2 ));
+    }
+
+    public static double function2(double x){
+        return sin(x - pi/4)/(sin(2*x) +2+(1+sin(x)+cos(x)));
+    }
+
+    public static double function3(double x){
+        return  log(x)/Math.pow(x,3);
+    }
+
+    public static double leftRiemannSum(double n, double a, double b, int index){
+        double AreaLeft = 0;
+        double x;
+        for (int i = 0; i<n; i++) {
+            x = a + (i) + delta(a, b, n);
+            AreaLeft = getArea(n, a, b, index, AreaLeft, x);
+        }
+        return AreaLeft;
+    }
+
+    private static double getArea(double n, double a, double b, int index, double Area, double x) {
+        switch (index) {
+            case 1 -> Area = Area + delta(a, b, n) * function1(x);
+            case 2 -> Area = Area + delta(a, b, n) * function2(x);
+            case 3 -> Area = Area + delta(a, b, n) * function3(x);
+        }
+        return Area;
+    }
+
+    public static double rightRiemannSum(double n, double a, double b, int index){
+        double AreaRight = 0;
+        double x;
+        for (int i = 1; i<n; i++){
+            x = a + (i)*delta(a,a,n);
+            AreaRight = getArea(n, a, b, index, AreaRight, x);
+            AreaRight = AreaRight + delta(a,b,n) + function1(x);
+        }
+        return AreaRight;
+    }
+
+    public static double Average(double LeftRiemann, double RightRiemann){
+        return (LeftRiemann+RightRiemann)/2;
+    }
+
+}
---
+++ src/Integral/Main.java
@@ -0,0 +1,104 @@
+package Integral;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static double pi = 3.14;
+
+    public static void Menu(){
+        System.out.println("-------------------------------------------");
+        System.out.println("Calculate the integral of f(x)dx");
+        System.out.println("1. f(x) = [(4x-1)/(sqrt(2x+1)+2)]dx from 0 to 4");
+        System.out.println("2. f(x) = [sin(x-pi/4) / (sin2x + 2(1 + sinx + cosx))]dx from 0 to pi" );
+        System.out.println("3. f(x) = ln(x)/x³ dx from 1 to 2");
+        System.out.println("x. Exit");
+        System.out.println("Choose the function you want to integrate:\n");
+    }
+
+    public static void main(String[] args) throws Exception{
+
+
+        int indx;
+        int n = 1000;
+        String input = "";
+        String again = "";
+
+        Scanner scanner = new Scanner(System.in);
+        /*-----------------------------------------------------------
+        Written by Truong Lam Bao Hieu (27.01.2021)
+        ------------------------------------------------------------- */
+
+
+        Menu();
+
+
+        while(!input.equalsIgnoreCase("x")){
+            double a, b;
+            double answer;
+            double LeftRiemannSum, RightRiemannSum;
+            input = scanner.nextLine().trim();
+            switch (input) {
+                case "1":
+                    indx = 1 ;
+                    System.out.println("\nf(x) = [(4x-1)/(sqrt(2x+1)+2)]dx from 0 to 4");
+                    a = 0.0;
+                    b = 4.0;
+                    LeftRiemannSum = Function.leftRiemannSum(n,a,b, indx);
+                    RightRiemannSum = Function.rightRiemannSum(n,a,b, indx);
+                    answer = Function.Average(LeftRiemannSum,RightRiemannSum);
+                    System.out.println("\nIntegral ≈ " + answer);
+                    System.out.println("Do you want to continue? (Y/N)");
+                    break;
+
+
+                case "2":
+                    indx = 2;
+                    System.out.println("\nf(x) = [sin(x-pi/4) / (sin2x + 2(1 + sinx + cosx))]dx from 0 to pi");
+                    a = 0.0;
+                    b = pi;
+                    LeftRiemannSum = Function.leftRiemannSum(n,a,b,indx);
+                    RightRiemannSum = Function.rightRiemannSum(n,a,b, indx);
+                    answer = Function.Average(LeftRiemannSum,RightRiemannSum);
+                    System.out.println("\nIntegral ≈ " + answer);
+                    System.out.println("Do you want to continue? (Y/N)");
+                    again = scanner.nextLine().trim();
+                    if (again.equals("Y")){
+                        Menu();
+                    }
+                    else {
+                        break;
+                    }
+
+                case "3":
+                    indx = 3;
+                    System.out.println("\nf(x) = ln(x)/x³ dx from 1 to 2");
+                    a = 1.0;
+                    b = 2.0;
+                    LeftRiemannSum = Function.leftRiemannSum(n,a,b,indx);
+                    RightRiemannSum = Function.rightRiemannSum(n,a,b,indx);
+                    answer = Function.Average(LeftRiemannSum,RightRiemannSum);
+                    System.out.println("nIntegral ≈ " + answer);
+                    System.out.println("Do you want to continue? (Y/N)");
+                    again = scanner.nextLine().trim();
+                    if (again.equals("Y")){
+                        Menu();
+                    }
+                    else {
+                        break;
+                    }
+
+                case "x":
+                    break;
+
+                default:
+                    System.out.println("Your selection is not valid!");
+                    System.out.println("Please try again!\n\n");
+                    Menu();
+
+            }
+            System.out.println("\nSee you soon ^^");
+        }
+
+    }
+}
